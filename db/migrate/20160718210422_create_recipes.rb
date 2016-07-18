4d91f349eb3b1e2ebf0718b3c82ce83ca88baea5
depippo <jdepippo@gmail.com>
2016-07-18 17:33:18 -0400

    Created and ran migrations

--- db/migrate/20160718210422_create_recipes.rb
+++ db/migrate/20160718210422_create_recipes.rb
@@ -1,6 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
+      t.string :name
+      t.text :content
+      t.time :preptime
+      t.integer :servings
 
       t.timestamps null: false
     end
