56096bfb50f4f8929ad33e8addde3f8886679a4e
depippo <jdepippo@gmail.com>
2016-07-23 11:42:03 -0400

    Added Facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718213121) do
+ActiveRecord::Schema.define(version: 20160722205553) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 20160718213121) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
