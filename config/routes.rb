f807cdff04043534d2ed5b3a724ce2a8b6936e5b
depippo <jdepippo@gmail.com>
2016-07-28 18:42:01 -0400

    Reconfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :ingredients
-  resources :recipes do
-    resources :ratings
-    resources :photos
+  root 'recipes#index'
+  authenticate :user do
+    resources :ingredients
+    resources :recipes do
+      resources :ratings
+      resources :photos
+    end
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  #devise_scope :user do
-  #  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  #end
 end
