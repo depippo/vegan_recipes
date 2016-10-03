96d515ce62e85d87ba1a92c8b42928de57d6b887
depippo <jdepippo@gmail.com>
2016-10-03 12:54:12 -0400

    Prevent duplicate nutrient ingredients

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,6 +1,6 @@
 class Nutrient < ActiveRecord::Base
   has_many :nutrient_ingredients
   has_many :nutrient_recipes
-  has_many :ingredients, through: :nutrient_ingredients
+  has_many :ingredients, -> { uniq }, through: :nutrient_ingredients
   has_many :recipes, through: :nutrient_recipes
 end
