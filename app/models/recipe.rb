f8b07f21a0271eac24c225a99387a020de2059d4
depippo <jdepippo@gmail.com>
2016-10-04 08:38:25 -0400

    added favorite_recipe associations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
+  has_many :favorite_recipes
+  has_many :favorited_by, through: :favorite_recipes, source: :user
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   has_many :nutrients, -> { uniq }, :through => :ingredients, :class_name => "Nutrient"
