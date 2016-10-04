c24015074cdecad8423be8f0078010ffaac1a308
depippo <jdepippo@gmail.com>
2016-10-04 09:42:42 -0400

    Moved favorite view to partial, prevented duplicate user favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :recipes
   has_many :favorite_recipes
-  has_many :favorites, through: :favorite_recipes, source: :recipe
+  has_many :favorites, -> { uniq }, through: :favorite_recipes, source: :recipe
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
