f5687db7bd38a97c2c52f8eb584c6549044656e3
depippo <jdepippo@gmail.com>
2017-02-03 11:50:21 -0500

    Added user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+      respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @user }
+    end
   end
 
   def index
