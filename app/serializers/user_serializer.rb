348e8ff03f56cfc748dc0392d30747bd002221f4
depippo <jdepippo@gmail.com>
2017-02-06 15:39:40 -0500

    Jquery to cycle through username and user recipes on user show page

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
   def self.serialize(user)
+    user_recipes = []
+    user.recipes.each do |recipe|
+      user_recipes << recipe.name
+    end
     serialized_user = '{'
     serialized_user += '"id": ' + user.id.to_s + ', '
+    user_recipes.join(',')
+    serialized_user += '"recipes": ' + user_recipes.to_s + ','
     serialized_user += '"email": "' + user.email + '"'
     serialized_user += '}'
   end
