84623c8faa3cee73c7287c7a1d3405ba062a9694
depippo <jdepippo@gmail.com>
2017-02-10 14:07:10 -0500

    Cycling through users on show page now incorporates favorites recipes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -8,9 +8,18 @@ class UserSerializer < ActiveModel::Serializer
       user_recipes << recipe_hash
     end
     recipes_hash = user_recipes.to_json
+    user_favorites = []
+    user.favorites.each do |favorite|
+      favorite_hash = Hash.new
+      favorite_hash["id"] = favorite.id
+      favorite_hash["name"] = favorite.name
+      user_favorites << favorite_hash
+    end
+    favorites_hash = user_favorites.to_json
     serialized_user = '{'
     serialized_user += '"id": ' + user.id.to_s + ', '
     serialized_user += '"recipes": ' + recipes_hash + ','
+    serialized_user += '"favorites": ' + favorites_hash + ','
     serialized_user += '"email": "' + user.email + '"'
     serialized_user += '}'
   end
