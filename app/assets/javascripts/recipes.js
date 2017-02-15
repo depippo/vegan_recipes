64655570449243fb32e3650db209f60edb658b8c
depippo <jdepippo@gmail.com>
2017-02-15 16:57:53 -0500

    Cleaned up recipes.js, removed unnecessary console logs

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -2,52 +2,39 @@ $(document).on('turbolinks:load', function() {
   postComment();
 });
 
+function Comment(attributes){
+  this.name = attributes.name;
+  this.content = attributes.content;
+  this.id = attributes.id;
+}
 
-  // var source = $("#comment-template").html();
-  // var template = Handlebars.compile(source);
-  function Comment(attributes){
-    this.name = attributes.name;
-    this.content = attributes.content;
-    this.id = attributes.id;
-  }
+Comment.prototype.renderLi = function(){
+  var full_comment = "<p class='comment'>" + this.name + " says:<br>" + this.content + "</br> </p>";
+  $(".comments_container").append(full_comment)
+}
 
-  Comment.prototype.renderLi = function(){
-    var full_comment = "<p class='comment'>" + this.name + " says:<br>" + this.content + "</br> </p>";
-    $(".comments_container").append(full_comment)
-  }
+function postComment(){
+  $("form#new_comment").on("submit", function(e){
+    e.preventDefault();
+    var $form = $(this);
+    var action = $form.attr("action")
+    var params = $form.serialize()
 
-  function postComment(){
-    console.log("firing the postComment function.");
-    $("form#new_comment").on("submit", function(e){
-      e.preventDefault();
-      var $form = $(this);
-      var action = $form.attr("action")
-      var params = $form.serialize()
-
-      $.ajax({
-        url: action,
-        data: params,
-        dataType: "json",
-        method: "POST"
-      })
-      .success(function(json){
-        e.preventDefault();
-        var comment = new Comment(json);
-        var commentLi = comment.renderLi()
-        $("#comment_name").val('')
-        $("#comment_content").val('')
-      })
-      .error(function(response){
-        console.log("Broken", response)
-      })
+    $.ajax({
+      url: action,
+      data: params,
+       dataType: "json",
+      method: "POST"
     })
-  }
-
-
-
-// <script id="comment-template" type="text/x-handlebars-template">
-//   <p class="comment">
-//     {{name}} says: <br>
-//     {{content}} <br>
-//   </p>
-// </script>
+    .success(function(json){
+      e.preventDefault();
+      var comment = new Comment(json);
+      var commentLi = comment.renderLi()
+      $("#comment_name").val('')
+      $("#comment_content").val('')
+    })
+    .error(function(response){
+      console.log("Broken", response)
+    })
+  })
+}
