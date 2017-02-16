04e6b30764c2ab05653c74197414998603a6f080
depippo <jdepippo@gmail.com>
2017-02-15 21:16:48 -0500

    Moved JQuery for cycling through ingredients to ingredients.js file

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -1,19 +1,72 @@
-$(document).on('turbolinks:load', function() {
-  showNutrients();
-});
-
-function showNutrients() {
+function pageReady() {
   $(".js-shownutrients").one("click", function(event) {
-    var ul = event.target.parentNode;
-    var button = event.target;
     var id = $(this).data("id");
-    $.get("/ingredients/" + id + ".json").success(function(json) {
-      console.log(json.nutrients.length);
-      if (json.nutrients.length > 0) {
-        $(button).hide();
-        json.nutrients.forEach(function(nutrient){
-          $(ul).append('<li class="json_ingredients">' + nutrient.name + '</li>');
-      })}
-    });
+    showNutrients(event, id);
+  })
+  $(".js-next_ingredient").on("click", function(event) {
+    nextIngredient(event);
+  })
+  $(".js-previous_ingredient").on("click", function(event) {
+    previousIngredient(event);
+  }) 
+}
+
+function showNutrients(event, id) {
+  console.log("showNutrients");
+  var ul = event.target.parentNode;
+  var button = event.target;
+  $.get("/ingredients/" + id + ".json").success(function(json) {
+    console.log(json.nutrients.length);
+    if (json.nutrients.length > 0) {
+      $(button).hide();
+      json.nutrients.forEach(function(nutrient){
+        $(ul).append('<li class="json_ingredients">' + nutrient.name + '</li>');
+    })}
   });
 }
+
+function nextIngredient(event) {
+    event.preventDefault();
+    var nextId = parseInt($(".js-next_ingredient").attr("data-id")) + 1;
+    $.get("/ingredients/" + nextId + "/data").success(function(data) {
+      $(".ingredientName").text(data["name"]);
+      if (data["recipes"]) {
+        $(".ingredient_recipes").empty();
+        data["recipes"].forEach(function(element){
+          $(".ingredient_recipes").append('<li class="list"><a href="/recipes/' + element.id + '">' + element.name + '</a></li>');
+        })
+        $(".ingredient_nutrients").empty();
+        data["nutrients"].forEach(function(element){
+          $(".ingredient_nutrients").append('<li class="list"><a href="/nutrients/' + element.id + '">' + element.name + '</a></li>');
+        })
+        // re-set the id to current on the link
+        $(".js-next_ingredient").attr("data-id", data["id"]);
+      }
+    });
+};
+
+function previousIngredient(event) {
+    event.preventDefault();
+    var nextId = parseInt($(".js-next_ingredient").attr("data-id")) - 1;
+    $.get("/ingredients/" + nextId + "/data").success(function(data) {
+      $(".ingredientName").text(data["name"]);
+      if (data["recipes"]) {
+        $(".ingredient_recipes").empty();
+        data["recipes"].forEach(function(element){
+          $(".ingredient_recipes").append('<li class="list"><a href="/recipes/' + element.id + '">' + element.name + '</a></li>');
+        })
+      if (data["nutrients"]) {
+        $(".ingredient_nutrients").empty();
+        data["nutrients"].forEach(function(element){
+          $(".ingredient_nutrients").append('<li class="list"><a href="/nutrients/' + element.id + '">' + element.name + '</a></li>');
+        })
+      }
+        // re-set the id to current on the link
+        $(".js-next_ingredient").attr("data-id", data["id"]);
+      }
+    });
+};
+
+$(document).on('turbolinks:load', function() {
+  pageReady();
+});
